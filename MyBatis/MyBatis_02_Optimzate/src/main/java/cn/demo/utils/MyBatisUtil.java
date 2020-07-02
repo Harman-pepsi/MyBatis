ac98512aadc7726864415355f4ecd8a7d9f827c3
Harman <853696509@qq.com>
2020-07-02 16:35:14 +0800

    MyBatis(db.properties,alias)

---
+++ MyBatis/MyBatis_02_Optimzate/src/main/java/cn/demo/utils/MyBatisUtil.java
@@ -0,0 +1,35 @@
+package cn.demo.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @Classname MyBatisUtil
+ * @Description
+ * @Created by XJC·AW
+ * @Date 2020/7/2 16:06
+ * @Version V1.0.0
+ * @Since 1.0
+ */
+public class MyBatisUtil {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            String resource = "MyBatisConfig.xml";
+            InputStream resourceAsStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession getSqlSession(){
+        return sqlSessionFactory.openSession();
+    }
+}
---
+++ MyBatis/MyBatis_02_Optimzate/src/test/java/cn/demo/test/UserTest.java
@@ -0,0 +1,31 @@
+package cn.demo.test;
+
+import cn.demo.mapper.IUserMapper;
+import cn.demo.pojo.User;
+import cn.demo.utils.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @Classname UserTest
+ * @Description
+ * @Created by XJC·AW
+ * @Date 2020/7/2 16:09
+ * @Version V1.0.0
+ * @Since 1.0
+ */
+public class UserTest {
+
+    @Test
+    public void testUser(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        IUserMapper userMapper = sqlSession.getMapper(IUserMapper.class);
+        List<User> userAll = userMapper.getUserAll();
+        for (User user : userAll) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+}
